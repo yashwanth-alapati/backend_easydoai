d581cac15aea834ee3bf299ae3027aa708fd9791
yashwanth-alapati <yashalap6966@gmail.com>
2025-07-06 16:11:55 -0400

    FIX: Define SQLAlchemy Base in database.py to fix ImportError

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +9,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create the base class for your models to inherit from.
+Base = declarative_base()
